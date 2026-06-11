d59b62b5ab35c5f49cfab9761f28ca44d6cd8363
Frank <frank.r.mok@googmal.com>
2026-06-11 11:40:52 +0200

    Fix: Avatar useEffect sync

--- components/avatar-upload.tsx
+++ components/avatar-upload.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { PlayerProfile, uploadAvatar, updateAvatarUrl } from '@/lib'
 
 interface AvatarUploadProps {
